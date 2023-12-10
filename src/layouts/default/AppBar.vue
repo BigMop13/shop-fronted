84e1770ea185094d3477148503eaef7add771611
BigMop13 <kamil.kurek1212@gmail.com>
2023-12-10 15:27:14 +0100

    created eventBus for product categories

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,35 +1,31 @@
 <template>
-  <div class="text-center">
-    <v-select
-      v-model="location"
-      :items="locations"
-      label="Location"
-    ></v-select>
-    <v-menu :location="location">
-      <v-list>
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index"
-        >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </div>
+  <v-card>
+    <v-tabs
+      v-model="tab"
+      color="deep-purple-accent-4"
+      align-tabs="center"
+    >
+      <v-tab :value="1">Landscape</v-tab>
+      <v-tab :value="2">City</v-tab>
+      <v-tab :value="3">Abstract</v-tab>
+    </v-tabs>
+  </v-card>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      locations: [
-        'top',
-        'bottom',
-        'start',
-        'end',
-        'center',
-        ],
-      location: 'end',
-    }),
-  }
-</script>
+import EventBus from "@/plugins/eventBus";
 
+export default {
+  data: () => ({
+    tab: null,
+  }),
+
+  watch: {
+    tab(newValue) {
+      EventBus.emit('tabSelected', newValue)
+      console.log(newValue)
+    }
+  }
+}
+
+</script>
