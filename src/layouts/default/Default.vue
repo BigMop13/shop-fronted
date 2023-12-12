47c30844472a8ba491d5ac3665fae973b1efeccd
BigMop13 <kamil.kurek1212@gmail.com>
2023-12-13 00:15:14 +0100

    cleared, structured code

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,14 +1,13 @@
 <template>
   <v-app>
     <default-bar />
-
     <default-view />
     <default-footer />
   </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
-  import DefaultFooter from './Footer.vue'
+  import DefaultBar from '@/components/NavBar.vue'
+  import DefaultView from '@/components/Content.vue'
+  import DefaultFooter from '@/components/Footer.vue'
 </script>
