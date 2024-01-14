1fe0c6d2d46bf5da80003f8239951c90264aa9f3
BigMop13 <kamil.kurek1212@gmail.com>
2024-01-14 19:23:51 +0100

    dysplaing categories from api (replaced hardcoded version)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,9 +5,9 @@
       color="deep-purple-accent-4"
       align-tabs="center"
     >
-      <v-tab :value="1">Landscape</v-tab>
-      <v-tab :value="2">City</v-tab>
-      <v-tab :value="3">Abstract</v-tab>
+      <v-tab v-for="item in tabItems" :key="item.id" :value="item.id">
+        {{ item.name }}
+      </v-tab>
     </v-tabs>
   </v-card>
 </template>
@@ -18,12 +18,23 @@ import EventBus from "@/plugins/eventBus";
 export default {
   data: () => ({
     tab: null,
+    tabItems: [],
   }),
+
+  mounted() {
+    this.fetchData();
+  },
 
   watch: {
     tab(category) {
       EventBus.emit('tabSelected', {category: category})
-      console.log(category)
+    }
+  },
+
+  methods: {
+    async fetchData() {
+      const response = await fetch('http://206.81.30.52:8090/api/categories');
+      this.tabItems = await response.json();
     }
   }
 }
