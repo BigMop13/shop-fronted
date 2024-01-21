c1a2c82fc20a15e8e92c8dba67eb56aaa6a3f47f
BigMop13 <kamil.kurek1212@gmail.com>
2024-01-21 14:09:07 +0100

    added login and register functionality with saving token

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,13 @@
 <template>
   <form @submit.prevent="submit">
     <v-text-field
-      v-model="email.value.value"
+      v-model="email.value"
       :error-messages="email.errorMessage.value"
       label="E-mail"
     ></v-text-field>
 
     <v-text-field
-      v-model="password.value.value"
+      v-model="password.value"
       :error-messages="password.errorMessage.value"
       label="Password"
       type="password"
@@ -17,25 +17,43 @@
       class="me-4"
       type="submit"
     >
-      submit
+      Submit
     </v-btn>
 
     <v-btn @click="handleReset">
-      clear
+      Clear
     </v-btn>
   </form>
 </template>
 
 <script setup>
-
-import {useField} from 'vee-validate'
+import axios from 'axios';
+import { useField } from 'vee-validate';
 
 const email = useField('email');
 const password = useField('password');
 
-  const handleReset = () => {
-    email.resetField();
-    password.resetField();
-  };
+const submit = async () => {
+  try {
+    const env = import.meta.env.VITE_APP_API_BASE_URL;
+    console.log(email.value)
+    const response = await axios.post(env + 'login_check', {
+      username: email.value,
+      password: password.value
+    });
+    console.log(response.data.token);
+    localStorage.setItem('userToken', response.data.token);
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      alert('Incorrect email or password.');
+    } else {
+      alert('An error occurred. Please try again.');
+    }
+  }
+};
 
+const handleReset = () => {
+  email.resetField();
+  password.resetField();
+};
 </script>
